bcefda32a9212d1303a68a7ffc68817906a146bc
26-humam <26.humam@gmail.com>
2021-05-28 01:22:41 +0700

    ProyekKP

---
+++ application/views/global/home.php
@@ -0,0 +1,45 @@
+<div class="row">
+	<div class="col m 12">
+		<center>
+			<h5>Selamat Datang Di Perpustakaan Pasca Sarjana UIN SGD Bandung</h5>
+		</center><hr>
+	</div>
+
+	<div class="row">
+		<div class="col m12">
+			<div class="card-panel teal darken-2 center white-text">
+				<p>
+				Menuju Smart Library 2025
+				</p>
+				<p>
+				yang Unggul dan Kompetitif di Asia Tenggara
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/cc.jpg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/profile">Tentang Perpustakaan</a>
+				</div>
+			</div>
+		</div>
+
+        <div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/aa.jpeg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/list_buku">Daftar Buku</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+</div>
